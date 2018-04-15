2bdf63d414db37b5520a2feccc94455a8148fca7
Hrvoje <hrv.onlinework@gmail.com>
2018-04-15 19:40:30 +0200

    password reset default override

--- app/User.php
+++ app/User.php
@@ -10,6 +10,17 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new \App\Notifications\MailResetPasswordNotification($token));
+    }
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
